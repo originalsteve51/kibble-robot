b7cef849ecabb0aa0c8c51c9908941b4c87c31af
Stephen Harding <ssh751@gmail.com>
2021-09-08 12:04:36 -0400

    Testing 1,2,3...

--- temp-dev-json/test-json.py
+++ temp-dev-json/test-json.py
@@ -1,13 +1,22 @@
 import json
-import datetime
+from datetime import datetime
+
+"""
+cd = datetime.datetime.now()
+current_date = cd.strftime("%m/%d/%Y")
+current_time = cd.strftime("%H:%M")
+"""
+
+current_date = datetime.strftime(datetime.now(),"%m/%d/%Y")
+current_time = datetime.strftime(datetime.now(),"%H:%M") 
 
 run_data = dict()
-run_data['feed_0'] = dict()
-run_data['feed_0']['start_feed'] = str(datetime.datetime.now())
-run_data['feed_0']['start_weight'] = .023
-run_data['feed_0']['end_weight'] = .102
-run_data['feed_0']['stable_weight'] = .107
-run_data['feed_0']['details'] = [.023, .023, .023, .037, .054, .075, .102]
+run_data[current_date] = dict()
+run_data[current_date]['start_feed'] = current_time
+run_data[current_date]['start_weight'] = .023
+run_data[current_date]['end_weight'] = .102
+run_data[current_date]['stable_weight'] = .107
+run_data[current_date]['details'] = [.023, .023, .023, .037, .054, .075, .102]
 
 j_str = json.dumps(run_data)
 
@@ -15,4 +24,4 @@ print(j_str)
 
 new_run_data = json.loads(j_str)
 
-print(new_run_data['feed_0']['details'])
+print(new_run_data[current_date]['details'])
