e5df5b12992d813fab994e89c2312d220e4c41b0
Stephen Harding <ssh751@gmail.com>
2021-09-11 21:33:14 -0400

    Copy full size status.json to server

--- copy-status.sh
+++ copy-status.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-tail -100 ./status.json > ./feedtail.out
-sshpass -p 'pi-pass' scp ./feedtail.out pi@192.168.1.30:/home/pi/mycode/webapp 
+#tail -100 ./status.json > ./feedtail.out
+sshpass -p 'pi-pass' scp ./status.json pi@192.168.1.30:/home/pi/mycode/webapp 
 
